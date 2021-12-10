0adc7f474e2aee65f30a7a4c31bdf48eb2adff70
Shadab <shadab@ehostinguk.com>
2021-12-10 10:31:00 +0530

    banner changes

--- src/components/banner.js
+++ src/components/banner.js
@@ -31,8 +31,7 @@ const Banner = () => {
 
                 <
                 div className = "banner swiper-container" >
-                <
-                Swiper spaceBetween = { 0 }
+                <Swiper spaceBetween = { 0 }
                 slidesPerView = { 1 }
                 loop = { true }
                 speed = { 1500 }
@@ -42,105 +41,38 @@ const Banner = () => {
                 }
                 onSwiper = {
                     (swiper) => console.log(swiper)
-                } >
+                } 
+                >
 
-                <
-                SwiperSlide >
-                <
-                div className = "wdt" >
-                <
-                Text data - swiper - parallax = "-200%" > Hello there welcome to react world < /Text> { / * < p className = "contant"
-                data - swiper - parallax = "-180%" > We’ ve developed a suite of products and services designed to meet our client’ s specific requirements and help achieve their financial goals. < /p> */
-            } <
-            img className = "img"
-        data - swiper - parallax = "-50%"
-        src = { banner1 }
-        style = {
-            { width: "100%" }
-        }
-        />   <
-        img className = "img parallax-img"
-        src = { banner2 }
-        style = {
-            { width: "100%" }
-        }
-        />   < /
-        div > <
-            /SwiperSlide> <
-        SwiperSlide >
-            <
-            div className = "wdt" >
-            <
-            Text data - swiper - parallax = "-200%" > Hello there welcome to react world < /Text> { / * < p className = "contant"
-        data - swiper - parallax = "-180%" > We’ ve developed a suite of products and services designed to meet our client’ s specific requirements and help achieve their financial goals. < /p> */
-    } <
-    img className = "img"
-data - swiper - parallax = "-50%"
-src = { banner1 }
-style = {
-    { width: "100%" }
-}
-/>   <
-img className = "img parallax-img"
-src = { banner2 }
-style = {
-    { width: "100%" }
-}
-/>    < /
-div > <
-    /SwiperSlide> <
-SwiperSlide >
-    <
-    div className = "wdt" >
-    <
-    Text data - swiper - parallax = "-200%" > Hello there welcome to react world < /Text> { / * < p className = "contant"
-data - swiper - parallax = "-180%" > We’ ve developed a suite of products and services designed to meet our client’ s specific requirements and help achieve their financial goals. < /p> */
-} <
-img className = "img"
-data - swiper - parallax = "-50%"
-src = { banner1 }
-style = {
-    { width: "100%" }
-}
-/>   <
-img className = "img parallax-img"
-src = { banner2 }
-style = {
-    { width: "100%" }
-}
-/>   < /
-div > <
-    /SwiperSlide> <
-SwiperSlide >
-    <
-    div className = "wdt" >
-    <
-    Text data - swiper - parallax = "-200%" > Hello there welcome to react world < /Text> { / * < p className = "contant"
-data - swiper - parallax = "-180%" > We’ ve developed a suite of products and services designed to meet our client’ s specific requirements and help achieve their financial goals. < /p> */
-} <
-img className = "img"
-data - swiper - parallax = "-50%"
-src = { banner1 }
-style = {
-    { width: "100%" }
-}
-/>   <
-img className = "img parallax-img"
-src = { banner2 }
-style = {
-    { width: "100%" }
-}
-/>   < /
-div > <
-    /SwiperSlide>
 
-<
-/Swiper>  
+                    <SwiperSlide>
+                        <div>
+                        <img className = "img" data-swiper-parallax = "-50%" src = { banner1 } style = { { width: "100%" }}/>   
+                        <img className = "img parallax-img"  src = { banner2 } style = {{ width: "100%" }}/>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div>
+                        <img className = "img" data-swiper-parallax = "-50%" src = { banner1 } style = { { width: "100%" }}/>   
+                        <img className = "img parallax-img"  src = { banner2 } style = {{ width: "100%" }}/>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div>
+                        <img className = "img" data-swiper-parallax = "-50%" src = { banner1 } style = { { width: "100%" }}/>   
+                        <img className = "img parallax-img"  src = { banner2 } style = {{ width: "100%" }}/>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div>
+                        <img className = "img" data-swiper-parallax = "-50%" src = { banner1 } style = { { width: "100%" }}/>   
+                        <img className = "img parallax-img"  src = { banner2 } style = {{ width: "100%" }}/>
+                        </div>
+                    </SwiperSlide>
+                </Swiper>  
 
-<
-/div>
-)
-}
+</div>
+)}
 
 
 
